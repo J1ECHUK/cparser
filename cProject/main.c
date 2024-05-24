eaabff22c142be2d95396c2918cd103ad39cbd39
flamingo <kme.09@mail.ru>
2024-05-24 17:40:04 +0300

    temp test

--- cProject/main.c
+++ cProject/main.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <WinSock2.h>
+
+#difine BUFFER_SIZE 1024
 
 #include "dict_of_words.h"
 #include "dict_of_tags.h"
@@ -11,6 +14,60 @@
 #include "search_word_tags.h"
 
 int main() {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <url>\n", argv[0]);
+        return 1;
+    }
+
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        fprintf(stderr, "WSAStartup failed\n");
+        return 1;
+    }
+
+    SOCKET sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == INVALID_SOCKET) {
+        fprintf(stderr, "Error creating socket\n");
+        return 1;
+    }
+
+    struct hostent* server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr, "Error getting host by name\n");
+        return 1;
+    }
+
+    struct sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(80);
+    memcpy(&serverAddr.sin_addr.s_addr, server->h_addr, server->h_length);
+
+    if (connect(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+        fprintf(stderr, "Error connecting to server\n");
+        return 1;
+    }
+
+    char request[256];
+    sprintf(request, "GET / HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", argv[1]);
+    send(sockfd, request, strlen(request), 0);
+
+    char response[BUFFER_SIZE];
+    FILE* file = fopen("output.txt", "w");
+    if (file == NULL) {
+        fprintf(stderr, "Error opening file\n");
+        return 1;
+    }
+
+    int bytesRead;
+    while ((bytesRead = recv(sockfd, response, BUFFER_SIZE, 0)) > 0) {
+        fwrite(response, 1, bytesRead, file);
+    }
+
+    fclose(file);
+    closesocket(sockfd);
+    WSACleanup();
+
+
     char *filename = "html.txt";
     char *search_words = "OSINT osint";
     char *search_tags = "h1 h2 h3 p a";
