4a818ce7b6ff2a4ac84557cfea552033beacb768
flamingo <kme.09@mail.ru>
2024-05-25 20:45:36 +0300

    Сделала сокеты

--- cProject/main.c
+++ cProject/main.c
@@ -14,10 +14,6 @@
 #include "search_word_tags.h"
 
 int main() {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <url>\n", argv[0]);
-        return 1;
-    }
 
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
@@ -25,13 +21,14 @@ int main() {
         return 1;
     }
 
+    const char* url = "en.wikipedia.org";
     SOCKET sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == INVALID_SOCKET) {
         fprintf(stderr, "Error creating socket\n");
         return 1;
     }
 
-    struct hostent* server = gethostbyname(argv[1]);
+    struct hostent* server = gethostbyname(url);
     if (server == NULL) {
         fprintf(stderr, "Error getting host by name\n");
         return 1;
@@ -48,7 +45,7 @@ int main() {
     }
 
     char request[256];
-    sprintf(request, "GET / HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", argv[1]);
+    sprintf(request, "GET /wiki/Open-source_intelligence HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", url);
     send(sockfd, request, strlen(request), 0);
 
     char response[BUFFER_SIZE];
@@ -67,13 +64,12 @@ int main() {
     closesocket(sockfd);
     WSACleanup();
 
-
-    char *filename = "html.txt";
-    char *search_words = "OSINT osint";
-    char *search_tags = "h1 h2 h3 p a";
-    FILE *file = fopen(filename, "r");
+    char* filename = "output.txt";
+    char* search_words = "open-source intelligence";
+    char* search_tags = "h1 h2 p a";
+    FILE* file = fopen(filename, "r");
     if (!file) {
-        printf("Error while open file\n");
+        printf("Error while opening file\n");
         return 0;
     }
     Tags *tags = fill_tags(file);
