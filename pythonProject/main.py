90ea98aed2ac907c5adcaaacb72a8a4b12a38889
J1ECHUK <Aleksmoshnu152@gmail.com>
2024-05-26 20:30:09 +0300

    добавил на сервер странички

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -5,7 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('samsung.html')
+
+@app.route('/bruh/')
+def index2():
+    return render_template('blender.html')
+
+@app.route('/phuphayka/')
+def index3():
+    return render_template('nike.html')
+
+@app.route('/roma/')
+def index4():
+    return render_template('studiogusto.html')
 
 
 if __name__ == '__main__':
